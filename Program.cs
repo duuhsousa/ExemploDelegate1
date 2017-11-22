3c0cbe45747e97359e0848b0e3c80c8f2a0dba8b
EDUARDO DE SOUSA PEREIRA LIMA <31049529812@jpmorgan.sp>
2017-11-22 21:56:32 -0200

    FINAL

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExemploDelegate1
+{
+    class Program
+    {
+        public delegate void Cortador(string queijo);
+        public delegate string Novinha(string mortadela);
+        static void Main(string[] args)
+        {
+            /*Data("BACON");
+            System.Console.WriteLine(DataCompleta("BACON 2"));
+            */
+            
+            Cortador vera = new Cortador(Data);
+            vera("BACON ");
+            Novinha novinha = new Novinha(DataCompleta);
+            System.Console.WriteLine(novinha("BACON 2 "));
+        }
+
+        static void Data(string texto){
+            System.Console.WriteLine(texto+" Hoje eh "+ DateTime.Now.ToShortDateString());
+        }
+
+        static string DataCompleta(string nome){
+            return "Ola "+nome+" Hoje eh "+DateTime.Now.ToLongTimeString();
+        }
+    }
+}
